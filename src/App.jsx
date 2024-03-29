63cf4d947b2ec4846ff87218bb5024f36f714fab
Daophahut Mehalath <81461399+Daophahut1@users.noreply.github.com>
2024-03-29 14:05:35 +0000

    set homepage github to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Daophahut Mehalath",
+  title: "Developer",
+  email: "daophahut11@gmail.com",
+  gitHub: "Daophahut1",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "MEHALATH",
 };
 
 const primaryColor = "#4E567E";
